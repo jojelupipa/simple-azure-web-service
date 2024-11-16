b53ec71fead08ef0bbe563a75fdf8fbcdc00a86a
Jesús Sánchez de Lechina Tejada <jesus_38b@hotmail.com>
2024-11-16 13:18:26 +0100

    Fixed parameters of azure provisioning #2

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_resource_group" "rg" {
   location = "centralus"
 }
 
-resource "azurerm_app_service_plan" "app_service_plan" {
+resource "azurerm_service_plan" "app_service_plan" {
   name                = "simple-azure-web-app-service-plan-01"
   location            = "centralus"
   resource_group_name = azurerm_resource_group.rg.name
@@ -37,7 +37,11 @@ resource "azurerm_windows_web_app" "web_app" {
   name                = "simple-azure-web-app-app-01"
   location            = "centralus"
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_app_service_plan.app_service_plan.id
+  service_plan_id     = azurerm_service_plan.app_service_plan.id
+  
+  site_config {
+    python_version = "3.9"
+  }
 }
 
 resource "azurerm_mssql_server" "sql_server" {
@@ -45,22 +49,19 @@ resource "azurerm_mssql_server" "sql_server" {
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = "centralus"
   administrator_login          = "sqladmin"
-  administrator_login_password = var.sql_admin_password     # Defined in repo settings secrets
+  administrator_login_password = var.SQL_ADMIN_PASSWORD     # Defined in repo settings secrets
 }
 
 resource "azurerm_mssql_database" "sql_database" {
   name                = "simple-azure-web-app-database-01"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = "centralus"
-  server_name         = azurerm_sql_server.sql_server.name
+  server_id         = azurerm_sql_server.sql_server.id
   auto_pause_delay_in_minutes = 60
   sku_name            = "GP_S_Gen5_1"
 }
 
 resource "azurerm_mssql_firewall_rule" "allow_azure" {
   name                = "AllowAzure"
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name         = azurerm_sql_server.sql_server.name
+  server_id         = azurerm_sql_server.sql_server.id
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
 }
