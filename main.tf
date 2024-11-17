9d940266977c57424cd1645240097b43eb243d86
Jesús Sánchez de Lechina Tejada <jesus_38b@hotmail.com>
2024-11-17 13:48:41 +0100

    Fixing terraform parameters to match current version of azurerm (#5) (#6)
    Adding base infrastructure deployment
    
    * Updated name of Storage account for terraform status file
    
    * Fixed parameters of azure provisioning #2
    
    * Typo fix on main.tf #2
    
    * Fixing secret variable case + other parameters
    
    * Changed terraform version to last version
    
    * Adding settings for web app and sql server #2
    
    * Adapting to region quota for free tier and chaching DB capacity

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name   = "my-terraform-state-rg"
-    storage_account_name  = "myterraformstate"
+    storage_account_name  = "myterraformstate20241115"
     container_name        = "tfstate"
     key                   = "terraform.tfstate"
   }
@@ -18,49 +18,58 @@ provider "azurerm" {
   features {}
 }
 
+variable "sql_admin_password" {
+  description = "The administrator password for the Azure SQL Server."
+  type        = string
+  sensitive   = true
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "simple-azure-web-app-rg-01"
   location = "centralus"
 }
 
-resource "azurerm_app_service_plan" "app_service_plan" {
+resource "azurerm_service_plan" "app_service_plan" {
   name                = "simple-azure-web-app-service-plan-01"
-  location            = "centralus"
+  location            = "Australia Central"
   resource_group_name = azurerm_resource_group.rg.name
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  os_type             = "Linux"
+  sku_name            = "F1"
 }
 
 resource "azurerm_windows_web_app" "web_app" {
   name                = "simple-azure-web-app-app-01"
-  location            = "centralus"
+  location            = "Australia Central"
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_app_service_plan.app_service_plan.id
+  service_plan_id     = azurerm_service_plan.app_service_plan.id
+  
+  site_config {
+    always_on         = false
+  }
 }
 
 resource "azurerm_mssql_server" "sql_server" {
   name                         = "simple-azure-web-app-my-sql-server-01"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = "centralus"
+  version                      = "12.0"
   administrator_login          = "sqladmin"
   administrator_login_password = var.sql_admin_password     # Defined in repo settings secrets
+  minimum_tls_version          = "1.2"
+
 }
 
 resource "azurerm_mssql_database" "sql_database" {
-  name                = "simple-azure-web-app-database-01"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = "centralus"
-  server_name         = azurerm_sql_server.sql_server.name
+  name                        = "simple-azure-web-app-database-01"
+  server_id                   = azurerm_mssql_server.sql_server.id
   auto_pause_delay_in_minutes = 60
-  sku_name            = "GP_S_Gen5_1"
+  sku_name                    = "GP_S_Gen5_1"
+  min_capacity                = 0.5
 }
 
 resource "azurerm_mssql_firewall_rule" "allow_azure" {
   name                = "AllowAzure"
-  resource_group_name = azurerm_resource_group.rg.name
-  server_name         = azurerm_sql_server.sql_server.name
+  server_id         = azurerm_mssql_server.sql_server.id
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
 }
